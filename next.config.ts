63160791ee1faf852f417be0f39e4901c3ea78a9
LiCuru <ligiacmello@yahoo.com>
2025-01-26 17:47:29 -0300

    base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //output: 'export',
-  basePath : 'https://licuru.github.io/agroecology-ecommerce-ui',
+  basePath : '/agroecology-ecommerce-ui',
 
   images: {
     unoptimized: true,
